ec931a5acb9f7688d67595f6a3b9dc449b19c8aa
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2015-10-11 15:48:15 +0200

    Move Cocoa and HugoMDL down to noDemo
    No need to Blacklist.

--- generateThemeSite.sh
+++ generateThemeSite.sh
@@ -100,14 +100,14 @@ fi
 # html5: https://github.com/simonmika/hugo-theme-html5/issues/2
 # cocoa: missing static folder, //github.com/nishanths/cocoa-hugo-theme/pull/3
 # hugo-mdl: missing static folder: https://github.com/jchatkinson/HugoMDL/pull/6
-blacklist=('persona', 'html5', 'cocoa', 'hugo-mdl')
+blacklist=('persona', 'html5')
 
 # hugo-incorporated: too complicated, needs its own exampleSite: https://github.com/nilproductions/hugo-incorporated/issues/24
 # landing-page-hugo: same as above
 # still using deprecated vars (0.14): 'aglaus'
 # Journal: broken layout: TOD make issue
 # next: https://github.com/leopku/hugo-theme-next/issues/2
-noDemo=('hugo-incorporated', 'aglaus', 'journal')
+noDemo=('hugo-incorporated', 'aglaus', 'journal', 'cocoa', 'hugo-mdl')
 
 for x in `ls -d exampleSite/themes/*/ | cut -d / -f3`; do
   blacklisted=`echo ${blacklist[*]} | grep "$x"`
